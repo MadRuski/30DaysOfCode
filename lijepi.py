6a93473bcc4c4cff42d93f9f978b19b02b934df1
Matej Ban <matej.ban007@gmail.com>
2021-01-05 16:13:59 +0100

    Created Lijepi.py

---
+++ lijepi.py
@@ -0,0 +1,9 @@
+#HONI 2019/2020 1.kolo 2.zadatak Lijepi.py
+n=int(input())
+l=[]
+l1=[]
+for i in range(n):
+    l.append([])
+    l[i]=list(map(str,input().split()))
+    l1.append(int(l[i][0]+l[i][1]))
+print (sum(l1))
