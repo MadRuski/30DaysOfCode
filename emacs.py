6b48b949f81c27fae8cc1c2ac71adb390265c006
Matej Ban <47395251+creyten@users.noreply.github.com>
2021-01-14 00:53:57 +0100

    Add files via upload

---
+++ emacs.py
@@ -0,0 +1,23 @@
+x,y=map(int,input().split())
+l=[]
+for i in range(x):
+    l.append(list(str(input())))
+    #print(l[i])
+b=0
+for i in range(x):
+    for j in range(y):
+        if l[i][j]=='*':
+            l[i][j]=''
+            for ax in range(x-i):
+                if l[i+ax][j]=='.':
+                        break
+                for ay in range(y-j):
+                    if l[i+ax][j+ay]=='*':
+                        l[i+ax][j+ay]=''
+                    if l[i+ax][j+ay]=='.':
+                        break
+            b+=1
+            
+print(b)          
+                    
+            
