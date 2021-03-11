eaaa4f5205dde5faf20c9bea442af22931cdd529
Matej Ban <47395251+creyten@users.noreply.github.com>
2021-03-11 15:39:49 +0100

    Add files via upload

--- matura.py
+++ matura.py
@@ -1,9 +1,24 @@
-a=int(input())
-b=int(input())
-c=int(input())
-if c==0:
-	print(a-b)
-if b==0:
-	print(a-c)
-if a==0:
-	print(c+b)
+sat,minuta=map(int,input().split())
+vrijeme=int(input())
+u1=input().split(' ')
+u2=input().split(' ')
+u3=input().split(' ')
+u1=int(u1[0])*60+int(u1[1])
+u2=int(u2[0])*60+int(u2[1])
+u3=int(u3[0])*60+int(u3[1])
+v=sat*60+minuta
+v2=v+vrijeme
+if u1>=v+30 and u1<v2-15:
+	print('DA')
+else:
+	print('NE')
+
+if u2>=v+30 and u2<v2-15:
+	print('DA')
+else:
+	print('NE')
+
+if u3>=v+30 and u3<v2-15:
+	print('DA')
+else:
+	print('NE')
